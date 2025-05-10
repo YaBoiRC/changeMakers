d47cd6b9ba92dc27228b05ea12215061391942e4
Raymond Chavez <yaboircyt@gmail.com>
2025-05-09 21:54:52 -0600

    Initial Commit

---
+++ changeMakers/changeMakersApp.swift
@@ -0,0 +1,17 @@
+//
+//  changeMakersApp.swift
+//  changeMakers
+//
+//  Created by Raymond Chavez on 09/05/25.
+//
+
+import SwiftUI
+
+@main
+struct changeMakersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ changeMakersTests/changeMakersTests.swift
@@ -0,0 +1,17 @@
+//
+//  changeMakersTests.swift
+//  changeMakersTests
+//
+//  Created by Raymond Chavez on 09/05/25.
+//
+
+import Testing
+@testable import changeMakers
+
+struct changeMakersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
